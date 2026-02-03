0f29650ed401cc02bab9d2d20d74a10772400a70
Danila Mikhaliou <danila.mikhaliou@leverx.com>
2026-02-03 14:14:56 +0000

    Config for deploying

---
+++ app/storeservice/webapp/test/integration/FirstJourney.js
@@ -0,0 +1,37 @@
+sap.ui.define([
+    "sap/ui/test/opaQunit",
+    "./pages/JourneyRunner"
+], function (opaTest, runner) {
+    "use strict";
+
+    function journey() {
+        QUnit.module("First journey");
+
+        opaTest("Start application", function (Given, When, Then) {
+            Given.iStartMyApp();
+
+            Then.onTheStoresList.iSeeThisPage();
+
+        });
+
+
+        opaTest("Navigate to ObjectPage", function (Given, When, Then) {
+            // Note: this test will fail if the ListReport page doesn't show any data
+            
+            When.onTheStoresList.onFilterBar().iExecuteSearch();
+            
+            Then.onTheStoresList.onTable().iCheckRows();
+
+            When.onTheStoresList.onTable().iPressRow(0);
+            Then.onTheStoresObjectPage.iSeeThisPage();
+
+        });
+
+        opaTest("Teardown", function (Given, When, Then) { 
+            // Cleanup
+            Given.iTearDownMyApp();
+        });
+    }
+
+    runner.run([journey]);
+});
